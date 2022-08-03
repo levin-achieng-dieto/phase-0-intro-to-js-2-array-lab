d79ac1664885b7f9fa01cb47679bc19f1fb3622c
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-03 18:44:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(cat) {
+  cats.push(cat);
+}
+
+
+function destructivelyPrependCat(cat){
+  cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(){
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
+function appendCat(){
+  cats.length = 0;
+  cats.push('Milo', 'Otis', 'Garfield');
+  let cat = [...cats, "Broom"]
+  return cat
+}
+
+
+function prependCat(){
+  cats.length = 0;
+  cats.push('Milo', 'Otis', 'Garfield');
+  let cat = [ "Arnold", ...cats]
+  return cat
+}
+
+
+
+function removeFirstCat(){
+  cats.length = 0;
+  cats.push('Milo', 'Otis', 'Garfield');
+  return cats.slice(1)
+}
+
+function removeLastCat(){
+  cats.length = 0;
+  cats.push('Milo', 'Otis', 'Garfield');
+  console.log(cats.slice(0, 2));
+  return cats.slice(0, 2)
+}
+
